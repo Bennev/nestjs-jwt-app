cb2514310b124e64a0f03405d24f6516fa98e588
Matheus Benevides Militão <mb.militao@gmail.com>
2022-12-14 16:01:45 -0300

    Initial Commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { UsersModule } from './app/users/users.module';
+// import { ConfigModule } from '@nestjs/config';
+import { typeOrmConfig } from 'typeorm.config';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule],
+  controllers: [],
+  providers: [],
+})
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot(),
+//     TypeOrmModule.forRoot({
+//       type: process.env.TYPEORM_CONNECTION,
+//       host: process.env.TYPEORM_HOST,
+//       port: process.env.TYPEORM_POSRT,
+//       username: process.env.TYPEORM_USERNAME,
+//       password: process.env.TYPEORM_PASSWORD,
+//       database: process.env.TYPEORM_DATABASE,
+//       entities: [join(__dirname + '/**/*.entity{.js,.ts}')],
+//       synchronize: true,
+//     } as TypeOrmModuleOptions),
+//     UsersModule,
+//   ],
+//   controllers: [],
+//   providers: [],
+// })
+export class AppModule {}
