f53cab6ee72e4a6045ac9c66f853d5d7a09680b7
Matheus Benevides Militão <mb.militao@gmail.com>
2022-12-15 10:59:46 -0300

    updates

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -16,26 +16,30 @@ import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
 
-@Controller('api/v1/users')
-@UseGuards(AuthGuard('jwt'))
+@Controller('api/users')
+// @UseGuards(AuthGuard('jwt'))
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   async index() {
     return await this.usersService.findAll();
   }
 
-  @Post()
+  @Post('register')
   async store(@Body() body: CreateUserDto) {
     return await this.usersService.store(body);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
+  // @Get('home')
   async show(@Param('id', new ParseUUIDPipe()) id: string) {
     return await this.usersService.findOneByOrFail({ id });
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Put(':id')
   async update(
     @Param('id', new ParseUUIDPipe()) id: string, 
@@ -44,6 +48,7 @@ export class UsersController {
     return await this.usersService.update(id, body);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: true, credentials: true });
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })
   );
-  await app.listen(3000);
+  await app.listen(8000);
 }
 bootstrap();
