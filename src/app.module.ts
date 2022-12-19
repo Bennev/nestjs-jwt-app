f4d054b7e3dbe332f48017d0bdbc19ff754346ca
Matheus Benevides Militão <mb.militao@gmail.com>
2022-12-19 18:43:30 -0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,42 +1,35 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UsersModule } from './app/users/users.module';
-// import { ConfigModule } from '@nestjs/config';
-import { typeOrmConfig } from 'typeorm.config';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { CompaniesModule } from './app/companies/companies.module';
 import { LocationsModule } from './app/locations/locations.module';
 import { AccountablesModule } from './app/accountables/accountables.module';
 import { TicketsModule } from './app/tickets/tickets.module';
+import { join } from 'path'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), 
-    UsersModule, 
-    AuthModule, 
-    CompaniesModule, 
-    LocationsModule, 
-    AccountablesModule,
-    TicketsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: process.env.TYPEORM_PORT,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: true,
+    } as TypeOrmModuleOptions), 
+      UsersModule, 
+      AuthModule, 
+      CompaniesModule, 
+      LocationsModule, 
+      AccountablesModule,
+      TicketsModule],
   controllers: [],
   providers: [],
 })
 
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot(),
-//     TypeOrmModule.forRoot({
-//       type: process.env.TYPEORM_CONNECTION,
-//       host: process.env.TYPEORM_HOST,
-//       port: process.env.TYPEORM_POSRT,
-//       username: process.env.TYPEORM_USERNAME,
-//       password: process.env.TYPEORM_PASSWORD,
-//       database: process.env.TYPEORM_DATABASE,
-//       entities: [join(__dirname + '/**/*.entity{.js,.ts}')],
-//       synchronize: true,
-//     } as TypeOrmModuleOptions),
-//     UsersModule,
-//   ],
-//   controllers: [],
-//   providers: [],
-// })
 export class AppModule {}
--- typeorm.config.ts
+++ typeorm.config.ts
@@ -1,14 +1,15 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm'
-import { join } from 'path'
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+// import { join } from 'path'
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'mynestapi',
-  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
-  synchronize: true,
-}
+// export const typeOrmConfig:
+//   TypeOrmModuleOptions = {
+//   type: process.env.TYPEORM_CONNECTION,
+//   host: process.env.TYPEORM_HOST,
+//   port: process.env.TYPEORM_PORT,
+//   username: process.env.TYPEORM_USERNAME,
+//   password: process.env.TYPEORM_PASSWORD,
+//   database: process.env.TYPEORM_DATABASE,
+//   entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+//   synchronize: true,
+// }
 
